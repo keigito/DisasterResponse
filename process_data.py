1d9ce996007ae7826a7262792b61c0b453019e1b
keigito <keigito@gmail.com>
2019-01-30 18:08:05 -0600

    Refactored ETL and ML pipelines

--- process_data.py
+++ process_data.py
@@ -1,33 +1,72 @@
+import sys
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
 
-# load datasets from csv files
-messages = pd.read_csv("messages.csv")
-categories = pd.read_csv("categories.csv")
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
 
-# join the two df's
-df = messages.join(categories, rsuffix="_categories")
+    df = messages.join(categories, rsuffix="_categories")
+    
+    return df
+    
 
-# One-hot-encode the categories
-categories = df["categories"].str.split(";", expand=True) # Expand the category column to individual category columns
+def clean_data(df):
+    # One-hot-encode the categories
+    categories = df["categories"].str.split(";", expand=True) # Expand the category column to individual category columns
+    
+    # Extract the values from the category strings and enter the values into corresponding columns
+    category_colnames = lambda x: [str(y)[:-2] for y in x]
+    row = categories.iloc[0]
+    categories.columns = category_colnames(row)
+    
+    for column in categories.columns:
+        categories[column] = categories[column].apply(lambda x: str(x)[-1])
+        categories[column] = categories[column].apply(lambda x: int(x))
+    
+    # Clean up and add the categories df
+    df.drop(["id_categories", "categories"], axis=1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
 
-# Extract the values from the category strings and enter the values into corresponding columns
-category_colnames = lambda x: [str(y)[:-2] for y in x]
-row = categories.iloc[0]
-categories.columns = category_colnames(row)
+    # Drop duplicates
+    df.drop_duplicates(keep=False, inplace=True)
+    
+    return df
 
-for column in categories.columns:
-    categories[column] = categories[column].apply(lambda x: str(x)[-1])
-    categories[column] = categories[column].apply(lambda x: int(x))
 
-# Clean up and add the categories df
-df.drop(["id_categories", "categories"], axis=1, inplace=True)
-df = pd.concat([df, categories], axis=1)
+def save_data(df, database_filename):
+    # Save the df to the SQL db
+    db_path_name = 'sqlite:///' + database_filename
+    engine = create_engine(db_path_name)
+    df.to_sql('Messages_Categories', engine, index=False)
 
-# Drop duplicates
-df.drop_duplicates(keep=False, inplace=True)
 
-# Save the df to the SQL db
-engine = create_engine('sqlite:///DisasterResponse.db')
-df.to_sql('Messages_Categories', engine, index=False)
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
--- train_classifier.py
+++ train_classifier.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
@@ -14,10 +15,26 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.multioutput import MultiOutputClassifier
 import pickle
 import nltk
-nltk.download(['punkt', 'wordnet'])
+
+
+def load_data(database_filepath):
+    db_path_name = 'sqlite:///' + database_filepath
+    engine = create_engine(db_path_name)
+    df = pd.read_sql_table('Messages_Categories', engine)
+    
+    X = df.loc[:, "message"]
+    Y = df.iloc[:, 4:40]
+    
+    category_names = list(df.columns)
+    drop_names = ["id", "message", "original", "genre"]
+    for name in drop_names:
+        category_names.remove(name)
+    
+    return X, Y, category_names
+
 
 def tokenize(text):
-
+    
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -28,35 +45,79 @@ def tokenize(text):
 
     return clean_tokens
 
-if __name__ == "__main__":
 
-    engine = create_engine('sqlite:///DisasterResponse.db')
-    df = pd.read_sql_table('Messages_Categories', engine)
-
-    X = df.loc[:, "message"]
-    Y = df.iloc[:, 4:40]
-
-    X_train, X_test, y_train, y_test = train_test_split(X, Y)
+def build_model():
 
     pipeline = Pipeline([
         ('vectorizer', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
-        ("clf", MultiOutputClassifier(RandomForestClassifier(n_estimators=100), n_jobs=4))
+    #     (), # Feature engineering (word2vec/GloVe)
+        ("clf", MultiOutputClassifier(RandomForestClassifier(n_estimators=100), n_jobs=-1))
     ])
+    
+    return pipeline
 
-    parameters = {
-#    'vectorizer__ngram_range': ((1, 1), (1, 2)),
-#    'vectorizer__max_df': (0.5, 1.0),
-    'vectorizer__max_features': (5000, 10000), # 397
-    'tfidf__use_idf': (True, False), # 489 sec
-#    'clf__estimator__n_estimators': [100, 200], #
-    'clf__estimator__min_samples_split': [3, 4] # 444 sec
-    }
 
-    cv = GridSearchCV(pipeline, param_grid=parameters)
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    y_actu = Y_test.values
+    
+    results_dict = {}
+    for i in range(1, 37):
+        predicted = "pred_" + str(i)
+        actual = "actu_" + str(i)
+        pred_values = []
+        actu_values = []
+        for ii in range(len(y_pred)):
 
-    cv_results = cv.fit(X_train, y_train)
+            pred_values.append(int(y_pred[ii][i-1]))
+            actu_values.append(int(y_actu[ii][i-1]))
 
-    best_model = cv_results.best_estimator_
+        results_dict[predicted] = pred_values
+        results_dict[actual] = actu_values
+        
+    for i in range(1, 37):
+        pred = results_dict['pred_' + str(i)]
+        actu = results_dict['actu_' + str(i)]
 
-    pickle.dump(best_model, open("final_model.pickle", "wb"))
+        print("\n### " + category_names[i-1] + " ###\n")
+        print(classification_report(pred, actu))
+        
+        
+
+def save_model(model, model_filepath):
+
+    pickle.dump(model, open(model_filepath, "wb"))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        nltk.download(['punkt', 'wordnet'])
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
